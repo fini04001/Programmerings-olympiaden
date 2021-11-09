543e5123caa6b92c2267b74c787f10971fe16c1f
fini04001 <92377261+fini04001@users.noreply.github.com>
2021-11-09 15:34:14 +0100

    Add files via upload

---
+++ olympiaden uppgift 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace olympiaden_uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Kuvert = 0.074196;   // kuvert tot = 0.074196 * 2 /m^2
+            Double affischer = 0.12474;
+            Double informationsblad = 0.06237;
+            Double vikt = 0;
+            Double Kuvertvikt = 0;
+            Double affischervikt = 0;
+            Double infomationsvikt = 0;
+
+
+            Console.WriteLine("Kuvert ? ");
+            Kuvertvikt = int.Parse(Console.ReadLine());
+            Console.WriteLine("affischer ? ");
+            affischervikt = int.Parse(Console.ReadLine());
+            Console.WriteLine("infomationsblad ? ");
+            infomationsvikt = int.Parse(Console.ReadLine());
+            
+            
+            
+            vikt = ((Kuvertvikt*2 * Kuvert) + (affischer*2 * affischervikt) + (informationsblad * infomationsvikt));
+            Console.WriteLine(vikt);
+            
+
+
+
+        }
+    }
+}
