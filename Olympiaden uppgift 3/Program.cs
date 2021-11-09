20a4f672860b7cbf19bd88674f941e04cc60d94b
fini04001 <92377261+fini04001@users.noreply.github.com>
2021-11-09 16:29:46 +0100

    Add files via upload

---
+++ Olympiaden uppgift 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Olympiaden_uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+        Console.WriteLine("hur många med grönt kort?");
+        int antalN = int.Parse(Console.ReadLine());
+        Console.WriteLine("hur många utan grönt kort?");
+        int antalM = int.Parse(Console.ReadLine());
+
+        int tid = 10*(antalN/antalM+ 2*antalN/antalM); 
+       
+        Console.WriteLine(tid);
+
+        }
+    }
+}
